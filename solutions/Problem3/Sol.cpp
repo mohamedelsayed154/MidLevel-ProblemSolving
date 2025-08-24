16a546e0aa363215742b39831a5ef810b974afd1
Mohamed Ahmed <117952210+mohamedelsayed154@users.noreply.github.com>
2025-08-24 22:58:00 +0300

    Create Sol.cpp

---
+++ solutions/Problem3/Sol.cpp
@@ -0,0 +1,39 @@
+/*
+Problem #03
+  */
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+
+int main() {
+	srand(time(0));
+
+	int num[3][3];	
+	vector <int> arrSum(3, 0);
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			num[i][j] = rand() % 101;
+
+			cout << num[i][j] << "    ";
+		}
+		cout << endl;
+	}
+
+	cout << endl << endl;
+
+	for (int x = 0; x < 3; x++) {
+		int sum = 0;
+		for (int y = 0; y < 3; y++) {
+			
+			sum += num[x][y];
+		}
+		arrSum[x] = sum;
+	}
+
+	for (int w = 0; w < 3; w++) {
+		cout << "Row " << w + 1 << " Sum = " << arrSum[w] << endl;
+	}
+}
