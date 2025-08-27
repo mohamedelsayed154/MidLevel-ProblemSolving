88721dce23cf5291da2b4f189fd01f4a54c1ae1e
Mohamed Ahmed <117952210+mohamedelsayed154@users.noreply.github.com>
2025-08-27 15:40:54 +0300

    Create Sol.cpp

---
+++ solutions/Problem15/Sol.cpp
@@ -0,0 +1,39 @@
+/*
+Problem #15
+  */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[3][3];
+
+    // cin the matrix
+    cout << "Enter the 3x3 matrix elements :" << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << "Matrix : " << endl << endl;
+    for (int a = 0; a < 3; a++) {
+        for (int b = 0; b < 3; b++) {
+            cout << "  " << arr[a][b] << "  ";
+        }
+        cout << endl;
+    }
+    int num, count = 0;
+    cout << "Entre the number to count in the matrix : ";
+    cin >> num;
+
+    for (int x = 0; x < 3; x++) {
+        for (int y = 0; y < 3; y++) {
+            if (arr[x][y] == num) {
+                count++;
+            }
+        }
+    }
+
+    cout << "Number " << num << " count in matrix is " << count << endl;
+}
