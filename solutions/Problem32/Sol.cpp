96a3fc8a3b97b2e7269ef5aa1f4ad6af90109084
Mohamed Ahmed <117952210+mohamedelsayed154@users.noreply.github.com>
2025-09-04 00:53:57 +0300

    Create Sol.cpp

---
+++ solutions/Problem32/Sol.cpp
@@ -0,0 +1,33 @@
+/*
+Problem#32
+  */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	char s;
+	cout << "Entre the Character : ";
+	cin >> s;
+
+	switch (s) {
+	case 'a' :
+		cout << "Yes Letter '" << s << "' is Vowel\n";
+		break;
+	case 'e' : 
+		cout << "Yes Letter '" << s << "' is Vowel\n";
+		break;
+	case 'i' : 
+		cout << "Yes Letter '" << s << "' is Vowel\n";
+		break;
+	case 'o' : 
+		cout << "Yes Letter '" << s << "' is Vowel\n";
+		break;
+	case 'u' : 
+		cout << "Yes Letter '" << s << "' is Vowel\n";
+		break;
+
+	default : 
+		cout << "No it's not Vowel\n";
+	}
+}
