007d06c386c158c8ce1dbaa73958ac57ae9b612c
Muhammed Elsayed <117952210+mohamedelsayed154@users.noreply.github.com>
2025-09-04 21:50:34 +0300

    Create Sol.cpp

---
+++ solutions/Problem39/Sol.cpp
@@ -0,0 +1,34 @@
+/*
+Problem#39
+  */
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void StringAfterJoin(vector <string>& s , int count);
+
+int main() {
+	
+	int count;
+	cout << "Entre Number of Strings :";
+	cin >> count;
+	
+	vector <string> s(count);
+
+	for (int i = 0; i < count; i++) {
+		cout << "Entre String : ";
+		cin >> s[i];
+	}
+	cout << endl;
+	StringAfterJoin(s , count);
+}
+
+void StringAfterJoin(vector <string>& s , int count) {
+	cout << "String After Join : ";
+	for (int i = 0; i < count; i++) {
+		cout << s[i] << "  " ;
+	}
+	cout << endl;
+}
